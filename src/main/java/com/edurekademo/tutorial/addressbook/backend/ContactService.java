f934bc03a5c171341ae997390f6b0921cd43ad16
sidhu <sidhu@gmail.com>
2021-01-28 15:14:43 +0000

    Version 4 is releasing

--- src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/backend/ContactService.java
@@ -19,11 +19,11 @@ public class ContactService {
     static String[] fnames = { "Peter", "Alice", "John", "Mike", "Olivia",
             "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
-            "Jennifer", "Sai" };
+            "Jennifer" };
     static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
-            "Thompson", "Young", "King", "Robinson", "Prasanth" };
+            "Thompson", "Young", "King", "Robinson" };
 
     private static ContactService instance;
 
